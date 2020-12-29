6208d6bf0eff0fc4e15526d58b348f44632e9ece
tqa <trinhquocanh94@gmail.com>
2020-12-29 23:10:10 +0100

    Solve knapsack

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -22,44 +22,45 @@ def parse_input(input_data):
     return items, capacity
 
 
-def maximum_value(items, capacity, taken, item=None):
-    """Fill the knapsack with the highest values item."""
-    if capacity < 0:
-        taken[item.index] = 0
-        return -item.value
-    if not items or capacity == 0:
-        return 0, []
-    leave_item_value = maximum_value(capacity, items[1:])
-    take_item_value = (
-        maximum_value(capacity - items[0].weight, items[1:], items[0]) + items[0].value
-    )
-    if leave_item_value >= take_item_value:
-        taken[item.index] = 0
-        max_value = leave_item_value
-    else:
-        taken[item.index] = 1
-        max_value = take_item_value
-    return max_value, taken
+def maximum_value(items, capacity):
+    n = len(items)
+    K = [[0 for w in range(capacity + 1)] for i in range(n + 1)]
+    for i in range(n + 1):
+        for w in range(capacity + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif items[i - 1].weight <= w:
+                K[i][w] = max(
+                    items[i - 1].value + K[i - 1][w - items[i - 1].weight], K[i - 1][w]
+                )
+            else:
+                K[i][w] = K[i - 1][w]
 
-
-def solve_it(items, capacity):
-    """Naive solution for knapsack problem."""
-    value = 0
-    weight = 0
-    taken = [0] * len(items)
-
-    for item in items:
-        if weight + item.weight <= capacity:
-            taken[item.index] = 1
-            value += item.value
-            weight += item.weight
-    return value, taken
+    res = K[-1][-1]
+    taken = [0 for _ in items]
+    w = capacity
+    for i in range(n, 0, -1):
+        if res <= 0:
+            break
+        if res == K[i - 1][w]:
+            continue
+        else:
+            taken[i - 1] = 1
+            res = res - items[i - 1].value
+            w = w - items[i - 1].weight
+    return K[-1][-1], taken
 
 
 def print_output(value, taken):
     output_data = str(value) + " " + str(0) + "\n"
     output_data += " ".join(map(str, taken))
     return output_data
+
+
+def solve_it(input_data):
+    items, capacity = parse_input(input_data)
+    value, taken = maximum_value(items, capacity)
+    return print_output(value, taken)
 
 
 if __name__ == "__main__":
@@ -69,9 +70,7 @@ if __name__ == "__main__":
         file_location = sys.argv[1].strip()
         with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
-        items, capacity = parse_input(input_data)
-        value, taken = solve_it(items, capacity)
-        print(print_output(value, taken))
+        print(solve_it(input_data))
     else:
         print(
             "This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)"
--- knapsack/solver_test.py
+++ knapsack/solver_test.py
@@ -9,10 +9,10 @@ class Test(unittest.TestCase):
         with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
         items, capacity = parse_input(input_data)
-        value, taken = solve_it(items, capacity)
         value, taken = maximum_value(items, capacity)
-        self.assertEqual(value, 18)
-        self.assertEqual(taken, [1, 1, 0, 0])
+        print(taken)
+        self.assertEqual(value, 19)
+        self.assertEqual(taken, [0, 0, 1, 1])
 
 
 if __name__ == "__main__":
