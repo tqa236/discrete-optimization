ee2693aed64151ae3a389b91457a8c2d0cb2cc19
Quoc Anh Trinh <trinhquocanh94@gmail.com>
2021-01-01 10:37:10 +0100

    Solve Knapsack

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from collections import namedtuple
+from ortools.algorithms import pywrapknapsack_solver
 
 Item = namedtuple("Item", ["index", "value", "weight"])
 
@@ -22,38 +23,66 @@ def parse_input(input_data):
     return items, capacity
 
 
+def knapsack_solver(values, weights, capacities):
+    solver = pywrapknapsack_solver.KnapsackSolver(
+        pywrapknapsack_solver.KnapsackSolver.KNAPSACK_MULTIDIMENSION_BRANCH_AND_BOUND_SOLVER,
+        "KnapsackExample",
+    )
+
+    solver.Init(values, weights, capacities)
+    computed_value = solver.Solve()
+
+    packed_items = []
+    packed_weights = []
+    total_weight = 0
+    for i in range(len(values)):
+        if solver.BestSolutionContains(i):
+            packed_items.append(i)
+            packed_weights.append(weights[0][i])
+            total_weight += weights[0][i]
+    taken = [1 if index in packed_items else 0 for index in range(len(values))]
+    return computed_value, taken
+
+
 def maximum_value(items, capacity):
+    """Dynamic programming solution for 0/1 Knapsack with returned items.
+
+    This solution uses O(kN) memory and does not scale well.
+    """
     n = len(items)
-    print(items)
-    print(n)
-    optimal = [0]
-    value = [[0 for w in range(capacity + 1)] for _ in range(2)]
-    for i in range(1, n + 1):
-        current_weight = items[i - 1].weight
-        value[i % 2][1:current_weight] = value[(i - 1) % 2][1:current_weight]
-        for w in range(current_weight, capacity + 1):
-            value[i % 2][w] = max(
-                items[i - 1].value + value[(i - 1) % 2][w - current_weight],
-                value[(i - 1) % 2][w],
-            )
-        print(value[i % 2])
-        optimal.append(value[i % 2][-1])
-    res = value[n % 2][-1]
+    K = [[0 for w in range(capacity + 1)] for i in range(n + 1)]
+    for i in range(n + 1):
+        for w in range(capacity + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif items[i - 1].weight <= w:
+                K[i][w] = max(
+                    items[i - 1].value + K[i - 1][w - items[i - 1].weight], K[i - 1][w]
+                )
+            else:
+                K[i][w] = K[i - 1][w]
+
+    res = K[-1][-1]
     taken = [0 for _ in items]
-    print(len(optimal), res)
-    print(optimal)
+    w = capacity
     for i in range(n, 0, -1):
-        print(i, optimal[i - 1], res)
         if res <= 0:
             break
-        if res <= optimal[i - 1]:
+        if res == K[i - 1][w]:
             continue
         else:
             taken[i - 1] = 1
             res = res - items[i - 1].value
+            w = w - items[i - 1].weight
+    return K[-1][-1], taken
 
-    print(taken)
-    return value[n % 2][-1], taken
+
+def parse_or_tools_input(input_data):
+    items, capacity = parse_input(input_data)
+    values = [item.value for item in items]
+    weights = [[item.weight for item in items]]
+    capacities = [capacity]
+    return values, weights, capacities
 
 
 def print_output(value, taken):
@@ -64,7 +93,10 @@ def print_output(value, taken):
 
 def solve_it(input_data):
     items, capacity = parse_input(input_data)
-    value, taken = maximum_value(items, capacity)
+    values = [item.value for item in items]
+    weights = [[item.weight for item in items]]
+    capacities = [capacity]
+    value, taken = knapsack_solver(values, weights, capacities)
     return print_output(value, taken)
 
 
--- knapsack/solver_test.py
+++ knapsack/solver_test.py
@@ -1,10 +1,10 @@
 import unittest
 
-from solver import parse_input, solve_it, maximum_value
+from solver import parse_input, maximum_value, knapsack_solver, parse_or_tools_input
 
 
 class Test(unittest.TestCase):
-    def test_1(self):
+    def test_homemade(self):
         file_location = "data/ks_4_0"
         with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
@@ -13,48 +13,54 @@ class Test(unittest.TestCase):
         self.assertEqual(value, 19)
         self.assertEqual(taken, [0, 0, 1, 1])
 
+    def test_1(self):
+        file_location = "data/ks_4_0"
+        with open(file_location, "r") as input_data_file:
+            input_data = input_data_file.read()
+        values, weights, capacities = parse_or_tools_input(input_data)
+        value, taken = knapsack_solver(values, weights, capacities)
+        self.assertEqual(value, 19)
+        self.assertEqual(taken, [0, 0, 1, 1])
+
     def test_2(self):
         file_location = "data/ks_lecture_dp_2"
         with open(file_location, "r") as input_data_file:
             input_data = input_data_file.read()
-        items, capacity = parse_input(input_data)
-        value, taken = maximum_value(items, capacity)
-        weight = sum([items[i].weight for i, value in enumerate(taken) if value == 1])
+        values, weights, capacities = parse_or_tools_input(input_data)
+        value, taken = knapsack_solver(values, weights, capacities)
+        weight = sum(weights[0][i] for i, value in enumerate(taken) if value == 1)
         self.assertEqual(value, 44)
-        print(weight, capacity)
-        self.assertTrue(weight <= capacity)
+        self.assertTrue(weight <= capacities[0])
 
-    # def test_3(self):
-    #     file_location = "data/ks_19_0"
-    #     with open(file_location, "r") as input_data_file:
-    #         input_data = input_data_file.read()
-    #     items, capacity = parse_input(input_data)
-    #     value, taken = maximum_value(items, capacity)
-    #     weight = sum([items[i].weight for i, value in enumerate(taken) if value == 1])
-    #     self.assertEqual(value, 12248)
-    #     print(weight, capacity)
-    #     self.assertTrue(weight <= capacity)
+    def test_3(self):
+        file_location = "data/ks_19_0"
+        with open(file_location, "r") as input_data_file:
+            input_data = input_data_file.read()
+        values, weights, capacities = parse_or_tools_input(input_data)
+        value, taken = knapsack_solver(values, weights, capacities)
+        weight = sum(weights[0][i] for i, value in enumerate(taken) if value == 1)
+        self.assertEqual(value, 12248)
+        self.assertTrue(weight <= capacities[0])
 
-    # def test_4(self):
-    #     file_location = "data/ks_50_0"
-    #     with open(file_location, "r") as input_data_file:
-    #         input_data = input_data_file.read()
-    #     items, capacity = parse_input(input_data)
-    #     value, taken = maximum_value(items, capacity)
-    #     weight = sum([items[i].weight for i, value in enumerate(taken) if value == 1])
-    #     self.assertEqual(value, 142156)
-    #     print(weight, capacity)
-    #     self.assertTrue(weight <= capacity)
+    def test_4(self):
+        file_location = "data/ks_50_0"
+        with open(file_location, "r") as input_data_file:
+            input_data = input_data_file.read()
+        values, weights, capacities = parse_or_tools_input(input_data)
+        value, taken = knapsack_solver(values, weights, capacities)
+        weight = sum(weights[0][i] for i, value in enumerate(taken) if value == 1)
+        self.assertEqual(value, 142156)
+        self.assertTrue(weight <= capacities[0])
 
-    # def test_5(self):
-    #     file_location = "data/ks_200_0"
-    #     with open(file_location, "r") as input_data_file:
-    #         input_data = input_data_file.read()
-    #     items, capacity = parse_input(input_data)
-    #     value, taken = maximum_value(items, capacity)
-    #     self.assertEqual(value, 100236)
-    #     weight = sum([items[i].weight for i, value in enumerate(taken) if value == 1])
-    #     self.assertTrue(weight <= capacity)
+    def test_5(self):
+        file_location = "data/ks_200_0"
+        with open(file_location, "r") as input_data_file:
+            input_data = input_data_file.read()
+        values, weights, capacities = parse_or_tools_input(input_data)
+        value, taken = knapsack_solver(values, weights, capacities)
+        weight = sum(weights[0][i] for i, value in enumerate(taken) if value == 1)
+        self.assertEqual(value, 100236)
+        self.assertTrue(weight <= capacities[0])
 
 
 if __name__ == "__main__":
